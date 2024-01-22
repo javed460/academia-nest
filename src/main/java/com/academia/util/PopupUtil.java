91fadca082c80f506249d7ccd890a95d01e54431
Javed Bagwan <javed460@gmail.com>
2024-01-22 08:27:54 +0530

    added util class

---
+++ src/main/java/com/academia/util/PopupUtil.java
@@ -0,0 +1,25 @@
+package com.academia.util;
+
+import javafx.scene.control.Alert;
+
+import static javafx.scene.control.Alert.AlertType.ERROR;
+
+public final class PopupUtil {
+
+    public static void showLoginErrorPopup() {
+        Alert alert = new Alert(ERROR);
+        alert.setTitle("Error Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Invalid Username/Password");
+        alert.showAndWait();
+    }
+
+    public static void showSignUpErrorPopup() {
+        Alert alert = new Alert(ERROR);
+        alert.setTitle("Error Message");
+        alert.setHeaderText(null);
+        alert.setContentText("Please fill all fields");
+        alert.showAndWait();
+    }
+
+}
