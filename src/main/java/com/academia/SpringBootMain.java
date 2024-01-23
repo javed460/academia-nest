d8641372f7c53ab8f419038b7c1b120a6793cdc8
Javed Bagwan <javed460@gmail.com>
2024-01-23 07:55:24 +0530

    Springboot main class

--- src/main/java/com/academia/SpringBootMain.java
+++ src/main/java/com/academia/SpringBootMain.java
@@ -1,7 +1,12 @@
 package com.academia;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static javafx.application.Application.launch;
+
+@SpringBootApplication
 public class SpringBootMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(JavaFxApplication.class, args);
     }
 }
