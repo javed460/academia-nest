719d5df6f694617c301a3ed8e289e811fca052d6
Javed Bagwan <javed460@gmail.com>
2024-01-28 08:48:04 +0530

    Updated LoginController

--- src/main/java/com/academia/controller/LoginController.java
+++ src/main/java/com/academia/controller/LoginController.java
@@ -4,14 +4,21 @@ import com.academia.model.Users;
 import com.academia.service.UsersService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -55,12 +62,39 @@ public class LoginController implements Initializable {
     public void loginAdmin(ActionEvent actionEvent) {
         Optional<Users> user = usersService.getUser(username.getText(), password.getText());
         if (user.isPresent()) {
-            System.out.println("User exists");
+            loginBtn.getScene().getWindow().hide();
+            loadDashboard();
         } else {
             showLoginErrorPopup();
         }
     }
 
+    private void loadDashboard() {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/main-dashboard.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+
+        root.setOnMousePressed((MouseEvent event) ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void close(ActionEvent actionEvent) {
         System.exit(0);
